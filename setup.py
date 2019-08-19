681e75d9d5b87c4b5a512d8c439c48adf36eb9e9
Giacomo Tagliabue <giacomo.tag@gmail.com>
2019-08-19 17:56:14 +0200

    0.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def system(command):
 
 setup(
     name="PyKubeCtl",
-    version='0.1.6',
+    version='0.2.0',
     description="A python bridge to kubectl",
     url='https://github.com/4Catalyzer/pykubectl',
     author="Giacomo Tagliabue",
