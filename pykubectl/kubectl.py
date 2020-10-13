e5cd69a878d26e899d95ef57485497d045ace62e
Stephen Liu <sliu@quantum-si.com>
2020-10-13 15:29:22 -0400

    output pod logs at debug level on successful execution

--- pykubectl/kubectl.py
+++ pykubectl/kubectl.py
@@ -1,38 +1,45 @@
 import json
 import logging
 from subprocess import CalledProcessError, check_output
+import warnings
 
 
-class KubeCtl(object):
-    def __init__(self, bin='kubectl', global_flags=''):
-        super(KubeCtl, self).__init__()
-        self.kubectl = '{} {}'.format(bin, global_flags)
+class KubeCtl:
+    def __init__(self, bin="kubectl", global_flags=""):
+        super().__init__()
+        self.kubectl = f"{bin} {global_flags}"
 
-    def execute(self, command, definition=None, safe=False):
-        cmd = '{} {}'.format(self.kubectl, command)
+    def execute(self, command, definition=None, safe=False, ignore_errors=False):
+        cmd = f"{self.kubectl} {command}"
 
         if definition:
-            pre = 'echo \'{}\''.format(definition)
-            cmd = '{} | {} -f -'.format(pre, cmd)
+            pre = f"echo '{definition}'"
+            cmd = f"{pre} | {cmd} -f -"
 
-        logging.debug('executing {}'.format(cmd))
+        logging.debug(f"executing {cmd}")
 
         try:
             return check_output(cmd, shell=True)
         except CalledProcessError as e:
-            if not safe:
+            if safe:
+                warnings.warn(
+                    "Use ignore_errors instead of safe",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+            if not (safe or ignore_errors):
                 raise e
-            logging.warn('Command {} failed, swallowing'.format(command))
+            logging.warn(f"Command {command} failed, swallowing")
 
     def apply(self, *args, **kwargs):
-        return self.execute('apply', *args, **kwargs)
+        return self.execute("apply", *args, **kwargs)
 
     def delete(self, *args, **kwargs):
-        return self.execute('delete', *args, **kwargs)
+        return self.execute("delete", *args, **kwargs)
 
     def get(self, *args, **kwargs):
-        result = self.execute('get -o json', *args, **kwargs).decode()
-        return json.loads(result)['items']
+        result = self.execute("get -o json", *args, **kwargs).decode()
+        return json.loads(result)["items"]
 
     def describe(self, *args, **kwargs):
-        return self.execute('describe', *args, **kwargs)
+        return self.execute("describe", *args, **kwargs)
